a1bea0b7ce1ccc17e744dce228d3b4c09d9f3023
leotsampaio <leotsampaio@gmail.com>
2025-05-06 10:51:57 -0300

    alterações no nome

--- nome.hpp
+++ nome.hpp
@@ -1,23 +1,22 @@
-#ifndef NOME_HPP_INCLUDED
-#define NOME_HPP_INCLUDED
+#ifndef DOMINIOS_HPP_INCLUDED
+#define DOMINIOS_HPP_INCLUDED
 #include <string>
 using namespace std;
 class Nome {
 
-    private:
-
-        string nome;
-        bool validar(string);
-
-    public:
-        bool setNome(string);
-        string getNome();
-    };
-
+private:
+    const string DEFAULT = "";
+    string nome;
+    bool validar(string);
+public:
+    bool setNome(string);
+    string getNome();
+};
 inline string Nome::getNome(){
     return nome;
 }
-#endif 
+#endif // DOMINIOS_HPP_INCLUDED
+
 
 
 
