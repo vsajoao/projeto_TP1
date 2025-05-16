12c509d2d20b1694ab8a9127a56ba5a8f5dbd6e7
João Vitor Frabis Zago <jvfzago@gmail.com>
2025-05-16 17:20:25 -0300

    Comentarios feitos em codigo_de_negociacao
    Todos os comentários finalizados

--- dominio/hpp/codigo_de_negociacao.hpp
+++ dominio/hpp/codigo_de_negociacao.hpp
@@ -7,18 +7,26 @@
 #include <string>
 using namespace std;
 
-/// O 
-/// 
+/// O código de negociação guarda uma string que identifica uma negociação de uma carteira, ele é parte integrante da entidade ordem
+/// A string do código segue as seguintes regras:
+/// Texto com até 12 caracteres.
+/// Cada caracter pode ser dígito (0-9), letra (A- Zou a-z) ou espaço em branco.
+
 class Codigo_de_Negociacao {
     private:
+        /// Armazena valor da codigo de negociação
         string codigo_de_negociacao;
+        /// Valida se string proposta atende à todos os requisitos, se não lança exceção invalid_argument
+        void validar(string);
     
     public:
         Codigo_de_Negociacao();
         Codigo_de_Negociacao(string);
+        // Retorna string codigo de Negociação
         string getCodigo();
+        /// Muda codigo de negociacao após validação, 
+        /// Retorna invalid_argument se não atender a requisitos e possui valor com qual requisito não foi atendido em seu valor
         void setCodigo(string);
-        void validar(string);
 
 
 };
